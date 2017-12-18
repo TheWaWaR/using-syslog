a107e811081d2adb37546bee91a22bb970a0fec4
Qian Linfeng <thewawar@gmail.com>
2017-12-18 18:05:20 +0800

    🎉 Basic idea works

--- examples/basic/tornado-app.py
+++ examples/basic/tornado-app.py
@@ -1,12 +1,39 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import logging
+import logging.config
+import logging.handlers
+
 import tornado.ioloop
 import tornado.web
+
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'local': {
+            'format': '%(asctime)s %(message)s',
+        }
+    },
+    'handlers': {
+        'syslog': {
+            'class': 'logging.handlers.SysLogHandler',
+            'facility': 'local6',
+            'address': '/dev/log',
+            'formatter': 'local',
+        },
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['syslog']
+    },
+}
+logging.config.dictConfig(LOGGING)
 
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
+        logging.info('get: Hello, world')
         self.write("Hello, world")
 
 
